ef36e1e236d577cb2ab794d2a7e97b587622eef5
EnzoFenato <enzofenato@gmail.com>
2026-06-09 20:30:36 -0300

    feat: implementação do Decorator Pattern

---
+++ src/decorator/NotifierDecorator.java
@@ -0,0 +1,15 @@
+package decorator;
+
+
+public abstract class NotifierDecorator implements Notifier {
+    protected Notifier wrappee;
+
+    public NotifierDecorator(Notifier wrappee) {
+        this.wrappee = wrappee;
+    }
+
+    @Override
+    public void send(String message) {
+        wrappee.send(message);
+    }
+}
